5d26446d575a1fbad6fccd6903256992b6c5b960
chu-young <ttheko101@gmail.com>
2021-06-29 16:47:53 +0200

    strict typing

---
+++ app.js
@@ -0,0 +1,6 @@
+var mario = 'chu';
+var age = 30;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(20));
---
+++ app.ts
@@ -0,0 +1,8 @@
+const mario:string = 'chu';
+let age = 30;
+
+const circ = (diameter:number) =>{
+return diameter * Math.PI;
+}
+
+console.log(circ(20))
