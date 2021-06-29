d9fa101be4de1cc69f7a49549d36fca01c3e99c6
chu-young <ttheko101@gmail.com>
2021-06-29 16:56:31 +0200

    arrays and objects

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var mario = 'chu';
-var age = 30;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// const mario:string = 'chu';
+// let age = 30;
+// const circ = (diameter:number) =>{
+// return diameter * Math.PI;
+// }
+// console.log(circ(20))
+var names = ['thabo', 'thando', 'vanilla'];
+names.push('homer');
+//Mixed Arrays
+//Must declare all different types in initial array
+var mixed = [1, 'thabo', true, 'yes'];
+//object
+//cant add on extra properties once the object is defined
+//must have same keys if wanting to re-define
+var chu = {
+    name: 'chu',
+    age: 2,
+    fromSA: true
 };
-console.log(circ(20));
--- app.ts
+++ app.ts
@@ -1,8 +1,24 @@
-const mario:string = 'chu';
-let age = 30;
+// const mario:string = 'chu';
+// let age = 30;
 
-const circ = (diameter:number) =>{
-return diameter * Math.PI;
+// const circ = (diameter:number) =>{
+// return diameter * Math.PI;
+// }
+
+// console.log(circ(20))
+
+let names = ['thabo', 'thando','vanilla']
+names.push('homer')
+
+//Mixed Arrays
+//Must declare all different types in initial array
+let mixed = [1, 'thabo', true, 'yes']
+
+//object
+//cant add on extra properties once the object is defined
+//must have same keys if wanting to re-define
+let chu ={
+    name:'chu',
+    age: 2,
+    fromSA:true,
 }
-
-console.log(circ(20))
